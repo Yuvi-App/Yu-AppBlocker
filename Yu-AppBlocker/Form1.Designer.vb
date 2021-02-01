3f61dea6bac68c1797196a40914f93e10d362668
Yuvi <yuvi@yuvi.app>
2021-02-01 14:18:50 -0500

    First & Final?

---
+++ Yu-AppBlocker/Form1.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtEXEAdd = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lbxBlockedApps = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.BlockAppTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Watcher = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.QuitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.WatcherTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'txtEXEAdd
+        '
+        Me.txtEXEAdd.Location = New System.Drawing.Point(12, 25)
+        Me.txtEXEAdd.Name = "txtEXEAdd"
+        Me.txtEXEAdd.Size = New System.Drawing.Size(141, 20)
+        Me.txtEXEAdd.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Input EXE Name"
+        '
+        'lbxBlockedApps
+        '
+        Me.lbxBlockedApps.FormattingEnabled = True
+        Me.lbxBlockedApps.Location = New System.Drawing.Point(12, 103)
+        Me.lbxBlockedApps.Name = "lbxBlockedApps"
+        Me.lbxBlockedApps.Size = New System.Drawing.Size(141, 108)
+        Me.lbxBlockedApps.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(37, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Current Block Apps"
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(40, 51)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 1
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'BlockAppTimer
+        '
+        Me.BlockAppTimer.Interval = 5000
+        '
+        'Watcher
+        '
+        Me.Watcher.Icon = CType(resources.GetObject("Watcher.Icon"), System.Drawing.Icon)
+        Me.Watcher.Text = "Watcher"
+        Me.Watcher.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem, Me.QuitToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(104, 48)
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.OpenToolStripMenuItem.Text = "Open"
+        '
+        'QuitToolStripMenuItem
+        '
+        Me.QuitToolStripMenuItem.Name = "QuitToolStripMenuItem"
+        Me.QuitToolStripMenuItem.Size = New System.Drawing.Size(103, 22)
+        Me.QuitToolStripMenuItem.Text = "Quit"
+        '
+        'WatcherTimer
+        '
+        Me.WatcherTimer.Enabled = True
+        Me.WatcherTimer.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnAdd
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(165, 223)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lbxBlockedApps)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtEXEAdd)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "AppBro"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtEXEAdd As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lbxBlockedApps As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents BlockAppTimer As Timer
+    Friend WithEvents Watcher As NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents QuitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents WatcherTimer As Timer
+End Class
---
+++ Yu-AppBlocker/Form1.vb
@@ -0,0 +1,235 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Dim envpath = Environment.ExpandEnvironmentVariables("%APPDATA%")
+    Dim configFile = "config.ini"
+    Dim setPassword As String = ""
+    Dim base64encoded As String = vbNull
+    Dim passwordset As Boolean = False
+    Dim p() As Process
+    Dim BlockedAppsList As New List(Of String)
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Computer.FileSystem.FileExists(configFile) = False Then
+            MessageBox.Show("First run deteceted, please input a Parental Password")
+            Dim InputParentalPassword = InputBox("Enter a Password", "Parental Password")
+            Dim result As DialogResult = MessageBox.Show("Do you want to use " + InputParentalPassword + " as the password?", "Verify Password", MessageBoxButtons.YesNo)
+            If result = DialogResult.Yes Then
+                'Base64 encode password
+                Dim data As Byte() = System.Text.ASCIIEncoding.ASCII.GetBytes(InputParentalPassword)
+                base64encoded = System.Convert.ToBase64String(data)
+
+                'SetPassword
+                setPassword = InputParentalPassword
+                passwordset = True
+
+                'Write to file
+                Dim file As System.IO.StreamWriter
+                System.IO.File.WriteAllText(configFile, "")
+                file = My.Computer.FileSystem.OpenTextFileWriter(configFile, True)
+                file.WriteLine("[ParentalPassword]")
+                file.WriteLine(base64encoded)
+                file.WriteLine("[Blocked Apps]")
+                file.Close()
+                WatcherTimer.Enabled = True
+                IO.File.SetAttributes(configFile, IO.FileAttributes.Hidden)
+            ElseIf result = DialogResult.No Then
+                MessageBox.Show("Please Restart App")
+                Me.Close()
+            End If
+
+        ElseIf My.Computer.FileSystem.FileExists(configFile) = True Then
+            'Read Config
+            Dim configallLines As String() = File.ReadAllLines(configFile)
+            base64encoded = configallLines(1)
+            Dim lineCount = File.ReadAllLines(configFile).Length
+            Dim count = 3
+            While Not lineCount = count
+                BlockedAppsList.Add(configallLines(count))
+                count = count + 1
+            End While
+
+            'Decode & Set Password
+            Dim data1 As Byte() = System.Convert.FromBase64String(base64encoded)
+            setPassword = System.Text.ASCIIEncoding.ASCII.GetString(data1)
+            passwordset = True
+
+            'Start Timer
+            WatcherTimer.Enabled = True
+
+            'BlockAppList
+            For Each item In BlockedAppsList
+                lbxBlockedApps.Items.Add(item)
+            Next
+        End If
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        Me.WindowState = FormWindowState.Minimized
+        Me.Hide()
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        BlockApp()
+    End Sub
+
+
+    ' Functions
+    Private Sub BlockApp()
+        Dim AppToBlock As String = txtEXEAdd.Text
+        If Not AppToBlock = "" Then
+            If passwordset = True Then
+                Dim InputPassword As String = InputBox("Enter Password", "Parental Password")
+                If InputPassword = setPassword Then
+                    Using sw As StreamWriter = File.AppendText(configFile)
+                        sw.WriteLine(AppToBlock)
+                    End Using
+                    lbxBlockedApps.Items.Add(AppToBlock)
+                    BlockedAppsList.Add(AppToBlock)
+                    txtEXEAdd.Clear()
+                Else
+                    MessageBox.Show("Password Incorrect")
+                End If
+            ElseIf passwordset = False Then
+                MessageBox.Show("Password not set, Please rerun app")
+            End If
+        Else
+            MessageBox.Show("Please input a exe")
+        End If
+    End Sub
+
+    Private Sub WatcherTimer_Tick(sender As Object, e As EventArgs) Handles WatcherTimer.Tick
+        Select Case Weekday(Now())
+            Case vbMonday
+                Dim curr As Date = Date.Now
+                Dim startTime As New Date(curr.Year, curr.Month, curr.Day, 4, 0, 0)
+                Dim endTime As New Date(curr.Year, curr.Month, curr.Day, 15, 0, 0)
+                If (curr >= startTime) And (curr <= endTime) Then
+                    BlockAppTimer.Enabled = True
+                Else
+                    BlockAppTimer.Enabled = False
+                End If
+
+            Case vbTuesday
+                Dim curr As Date = Date.Now
+                Dim startTime As New Date(curr.Year, curr.Month, curr.Day, 4, 0, 0)
+                Dim endTime As New Date(curr.Year, curr.Month, curr.Day, 15, 0, 0)
+                If (curr >= startTime) And (curr <= endTime) Then
+                    BlockAppTimer.Enabled = True
+                Else
+                    BlockAppTimer.Enabled = False
+                End If
+
+            Case vbWednesday
+                Dim curr As Date = Date.Now
+                Dim startTime As New Date(curr.Year, curr.Month, curr.Day, 4, 0, 0)
+                Dim endTime As New Date(curr.Year, curr.Month, curr.Day, 15, 0, 0)
+                If (curr >= startTime) And (curr <= endTime) Then
+                    BlockAppTimer.Enabled = True
+                Else
+                    BlockAppTimer.Enabled = False
+                End If
+
+            Case vbThursday
+                Dim curr As Date = Date.Now
+                Dim startTime As New Date(curr.Year, curr.Month, curr.Day, 4, 0, 0)
+                Dim endTime As New Date(curr.Year, curr.Month, curr.Day, 15, 0, 0)
+                If (curr >= startTime) And (curr <= endTime) Then
+                    BlockAppTimer.Enabled = True
+                Else
+                    BlockAppTimer.Enabled = False
+                End If
+
+            Case vbFriday
+                Dim curr As Date = Date.Now
+                Dim startTime As New Date(curr.Year, curr.Month, curr.Day, 4, 0, 0)
+                Dim endTime As New Date(curr.Year, curr.Month, curr.Day, 15, 0, 0)
+                If (curr >= startTime) And (curr <= endTime) Then
+                    BlockAppTimer.Enabled = True
+                Else
+                    BlockAppTimer.Enabled = False
+                End If
+
+            Case vbSaturday
+                BlockAppTimer.Enabled = False
+
+            Case vbSunday
+                BlockAppTimer.Enabled = False
+
+            Case Else
+                MessageBox.Show("Couldnt get day of week")
+                Me.Close()
+        End Select
+    End Sub
+
+    Private Sub BlockApp_Tick(sender As Object, e As EventArgs) Handles BlockAppTimer.Tick
+        'Timer Checks every 5 secs if App is running
+        Try
+            For Each proc1 As Process In Process.GetProcessesByName("Discord")
+                proc1.Kill()
+                Watcher.BalloonTipText = "Stay focused"
+                Watcher.ShowBalloonTip(5)
+            Next
+
+            For Each item In BlockedAppsList
+                For Each proc As Process In Process.GetProcessesByName(item)
+                    proc.Kill()
+                    Watcher.BalloonTipText = "Stay focused"
+                    Watcher.ShowBalloonTip(5)
+                Next
+            Next
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Form1_Resize(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Resize
+        If Me.WindowState = FormWindowState.Minimized Then
+            Watcher.Visible = True
+            Me.Hide()
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        Me.WindowState = FormWindowState.Minimized
+        Watcher.Visible = True
+        Me.Hide()
+        e.Cancel = True
+    End Sub
+
+    Private Sub Watcher_MouseClick(sender As Object, e As MouseEventArgs) Handles Watcher.MouseClick
+        ContextMenuStrip1.Show(MousePosition.X, MousePosition.Y)
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        If passwordset = True Then
+            Dim InputPassword As String = InputBox("Enter Password", "Parental Password")
+            If InputPassword = setPassword Then
+                Me.Show()
+                Me.WindowState = FormWindowState.Normal
+            Else
+                MessageBox.Show("Password Incorrect")
+            End If
+        End If
+    End Sub
+
+    Private Sub QuitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles QuitToolStripMenuItem.Click
+        If passwordset = True Then
+            Dim InputPassword As String = InputBox("Enter Password", "Parental Password")
+            If InputPassword = setPassword Then
+                Try
+                    For Each proc As Process In Process.GetProcessesByName("Yu-AppBlocker")
+                        proc.Kill()
+                    Next
+                    For Each proc As Process In Process.GetProcessesByName("AppBro")
+                        proc.Kill()
+                    Next
+                Catch ex As Exception
+
+                End Try
+            Else
+                MessageBox.Show("Password Incorrect")
+            End If
+        End If
+    End Sub
+End Class
